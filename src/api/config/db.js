d1ba1e194453e2c3e341436399c837a5339963f0
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2026-04-01 19:54:00 +0700

    ml service

--- src/api/config/db.js
+++ src/api/config/db.js
@@ -15,9 +15,9 @@ const sequelize = new Sequelize(
         timezone: '+07:00', 
 
         pool: {
-            max: 10,
+            max: 20,
             min: 0,
-            acquire: 30000,
+            acquire: 60000,
             idle: 10000
         }
     }
