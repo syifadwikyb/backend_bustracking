3adff647e2cf84d186e4a7e358e19c5e325bda4a
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2026-05-05 19:21:40 +0700

    testing

--- src/ws/socket.js
+++ src/ws/socket.js
@@ -13,38 +13,25 @@ const initSocket = (server) => {
     pingTimeout: 60000,
   });
 
-  // io.on("connection", (socket) => {
-  //   console.log(`Client connected: ${socket.id}`);
-
-  //   socket.on("join_bus_room", (busId) => {
-  //     socket.join(`bus_${busId}`);
-  //   });
-
-  //   socket.on("bus_location", (data) => {
-  //     socket.emit("bus_location_response", {
-  //       ...data,
-  //       server_time: Date.now(),
-  //     });
-  //   });
-
-  //   socket.on("bus_location_test", (data) => {
-  //     socket.emit("bus_location_response", {
-  //       status: "ok",
-  //       server_time: Date.now(),
-  //     });
-  //   });
-
-  //   socket.on("disconnect", () => {
-  //     console.log(`❌ Client disconnected: ${socket.id}`);
-  //   });
-  // });
-
   io.on("connection", (socket) => {
     console.log(`Client connected: ${socket.id}`);
 
-    socket.on("join_bus_room", (busId, callback) => {
+    socket.on("join_bus_room", (busId) => {
       socket.join(`bus_${busId}`);
-      if (callback) callback({ status: "ok" });
+    });
+
+    socket.on("bus_location", (data) => {
+      socket.emit("bus_location_response", {
+        ...data,
+        server_time: Date.now(),
+      });
+    });
+
+    socket.on("bus_location_test", (data) => {
+      socket.emit("bus_location_response", {
+        status: "ok",
+        server_time: Date.now(),
+      });
     });
 
     socket.on("disconnect", () => {
@@ -56,7 +43,12 @@ const initSocket = (server) => {
 };
 
 export const emitBusLocation = (data) => {
-  io.to(`bus_${data.bus_id}`).emit("bus_location", data);
+  if (io) {
+    console.log("📡 Emitting bus_location:", data.bus_id);
+    io.emit("bus_location", data);
+  } else {
+    console.log("❌ io belum init!");
+  }
 };
 
 export default initSocket;
