69f1867020a5e2ab432f98c021546ee48de048ad
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2026-05-05 14:43:02 +0700

    testing

--- src/ws/socket.js
+++ src/ws/socket.js
@@ -13,29 +13,53 @@ const initSocket = (server) => {
     pingTimeout: 60000,
   });
 
+  // io.on("connection", (socket) => {
+  //   console.log(`Client connected: ${socket.id}`);
+
+  //   socket.on("join_bus_room", (busId) => {
+  //     socket.join(`bus_${busId}`);
+  //   });
+
+  //   socket.on("bus_location", (data) => {
+  //     socket.emit("bus_location_response", {
+  //       ...data,
+  //       server_time: Date.now(),
+  //     });
+  //   });
+
+  //   socket.on("bus_location_test", (data) => {
+  //     socket.emit("bus_location_response", {
+  //       status: "ok",
+  //       server_time: Date.now(),
+  //     });
+  //   });
+
+  //   socket.on("disconnect", () => {
+  //     console.log(`❌ Client disconnected: ${socket.id}`);
+  //   });
+  // });
+
   io.on("connection", (socket) => {
     console.log(`Client connected: ${socket.id}`);
 
-    socket.on("join_bus_room", (busId) => {
+    // ⬇️ kirim data langsung saat connect
+    socket.emit("bus_location", {
+      bus_id: 1,
+      latitude: -7.05,
+      longitude: 110.43,
+      speed: 30,
+      penumpang: 1,
+      updated_at: new Date(),
+    });
+
+    socket.on("join_bus_room", (busId, callback) => {
       socket.join(`bus_${busId}`);
-    });
 
-    socket.on("bus_location", (data) => {
-      socket.emit("bus_location_response", {
-        ...data,
-        server_time: Date.now(),
-      });
-    });
-
-    socket.on("bus_location_test", (data) => {
-      socket.emit("bus_location_response", {
-        status: "ok",
-        server_time: Date.now(),
-      });
-    });
-
-    socket.on("disconnect", () => {
-      console.log(`❌ Client disconnected: ${socket.id}`);
+      if (callback) {
+        callback({ status: "ok" });
+      }
+      
+      socket.on("disconnect", () => clearInterval(interval));
     });
   });
 
